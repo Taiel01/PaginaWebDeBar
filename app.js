5e4543329f7ca4d7a0168e82221b0b086414ceba
Taiel <100381023+Taiel01@users.noreply.github.com>
2022-10-03 09:55:13 -0300

    v 1.0
    La pagina sin fallos 100% usable

---
+++ app.js
@@ -0,0 +1,62 @@
+
+let wsp = document.querySelector(".wsp");
+let nombre = document.querySelector(".nombre");
+let dia = document.querySelector(".dia");
+let hora = document.querySelector(".hora");
+let guardar = document.querySelector(".guardar");
+
+let reserva_nav =  document.querySelector(".reserva_nav");
+reserva_nav.addEventListener("click",irAReservas);
+
+let mapa_nav =  document.querySelector(".mapa_nav");
+mapa_nav.addEventListener("click",irAMap);
+
+let form = document.querySelector(".login-box");
+
+let carrusel = document.querySelector(".carousel-inner");
+
+let mapa = document.querySelector(".mapa");
+let top2 = mapa.getBoundingClientRect().top;
+let top3 = form.getBoundingClientRect().top;
+
+guardar.addEventListener("click",reservarEvento);
+
+function reservarEvento(){
+
+    wsp.outerHTML = `<a target=\"_blank\" class=\"wsp\" href=\"https://api.whatsapp.com/send?phone=3413157248&amp;text=Hola,quiero hacer una reserva a nombre de `+ nombre.value +` el dia `+ dia.value +` a las `+ hora.value +` \">Reserva</a>`
+
+    wsp.classList.remove("ocultar");
+    guardar.classList.add("ocultar");
+}
+
+window.addEventListener("scroll", detectarPosibleCambio);
+
+function detectarPosibleCambio(){
+    if(window.scrollY >= top2 / 6){
+        mapa.classList.remove("antiwidth");
+    }
+
+    if(window.visualViewport.pageTop >= top3 / 2){
+        form.classList.remove("opacity0");
+    }
+}
+
+window.addEventListener("load", gatos);
+
+function gatos(){
+    carrusel.classList.remove("carousel-inner-mil")
+}
+
+
+
+
+function irAReservas(){
+    window.scrollTo(0, 2000);
+}
+
+function irAMap(){
+    window.scrollTo(0, top2);
+}
+
+
+
